9a6023be39442d4ad8e7f0254cdaa6c1c5dead84
web-dot <cg.sudipc@gmail.com>
2021-01-08 17:24:07 +0530

    07

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 //invoke express and path
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var helmet = require('helmet');
 //access express
 var app = express();
 //access port
-var port = process.env.port || '8000';
+var port = process.env.PORT || '8000';
 //server activation
 app.listen(port, function(){
   console.log(`Listening at http://localhost${port}`);
 })
 //app configuration
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: false }));
